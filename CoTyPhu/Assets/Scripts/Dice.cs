d7effbd9f51726032e72cc4bf981c8e9339786ba
Kiet Nguyen Tuan <nguyentuankiet23031998@gmail.com>
2018-02-21 21:52:47 +0700

    Add Dice and Place Panel
    Add UI for dice and place panel

---
+++ CoTyPhu/Assets/Scripts/Dice.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour {
+
+    public List<Sprite> dices;
+
+    private Animator animator;
+
+    Image imgDice;
+
+    public Image panelDice;
+
+    public int randValue;
+
+	// Use this for initialization
+	void Start () {
+        randValue = 1;
+        imgDice = GetComponent<Image>();
+        animator = panelDice.GetComponent<Animator>();
+        imgDice.sprite = dices[Random.Range(0, dices.Count - 1)];
+	}
+	
+    public void Show()
+    {
+        animator.SetBool("isShow", true);
+        StartCoroutine(DoGetDice());
+    }
+
+    public void Hide()
+    {
+        animator.SetBool("isShow", false);
+        GameObject.Find("PanelPlace").GetComponent<PlaceManager>().Show();
+    }
+
+    
+
+    private IEnumerator DoGetDice()
+    {
+        yield return new WaitForSeconds(2);
+        int randSeed = Random.Range(10, 20);
+        int value = 0;
+        float diceSpeed = 0.1f;
+        for(int i = 0; i < randSeed; i++)
+        {
+            value = Random.Range(0, dices.Count - 1);
+            imgDice.sprite = dices[value];
+            yield return new WaitForSeconds(diceSpeed);
+            diceSpeed += 0.01f;
+        }
+        randValue = value + 1;
+        yield return new WaitForSeconds(4);
+        Hide();
+        yield break;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
---
+++ CoTyPhu/Assets/Scripts/PlaceManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceManager : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+
+    public void Show()
+    {
+        animator.SetBool("isShow", true);
+    
+    }
+    public void Hide()
+    {
+        animator.SetBool("isShow", false);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
